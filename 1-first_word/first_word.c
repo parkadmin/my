93c977b690af37b51eb96b1feb1a0951e20caf56
Chanel Jakubowski <cjakubow@e1r6p1.21-school.ru>
2018-10-08 18:27:41 +0300

    FW

--- 1-first_word/first_word.c
+++ 1-first_word/first_word.c
@@ -1,27 +1,23 @@
 #include <unistd.h>
-
-void ft_str(char *str)
+int main(int argc, char *argv[])
 {
 	int i;
-
 	i = 0;
-	while(str[i] != '\0')
+	if (argc == 2)
 	{
-		write(1, &str[i], 1);
-		i++;
+		while(argv[1][i] != '\0')
+		{
+			while (argv[1][i] == ' '|| argv[1][i] == '\t') 
+				i++;
+			if (argv[1][i] != ' '|| argv[1][i] != '\t')
+				{
+					while(argv[1][i] >= 33 && argv[1][i] <= 126)
+				  	write(1, "argv[1][i]\n", 1);
+					i++;
+				}
+		}
 	}
 	write(1, "\n", 1);
-}
-
-int main(int argc, char **argv)
-{
-	
-	if (argc < 2)
-	{	
-	write(1,"\n", 1);
-	}	
-else 
-	ft_str(argv[1]);
 	return(0);
 }
 
